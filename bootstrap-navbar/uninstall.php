34b01afa99f5e6138f6ec0ce0ffa3ecff43de572
Mathew Callaghan <mathewcallaghan@gmail.com>
2017-11-15 12:51:29 +1100

    Create uninstall.php

---
+++ bootstrap-navbar/uninstall.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ *
+ * @link       https://github.com/mathewcallaghan/
+ * @since      1.0.0
+ *
+ * @package    Bootstrap_Navbar
+ */
+
+// if uninstall.php is not called by WordPress, die
+if (!defined('WP_UNINSTALL_PLUGIN')) {
+    die;
+}
